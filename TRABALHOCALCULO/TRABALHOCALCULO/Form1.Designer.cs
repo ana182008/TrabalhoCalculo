e7a07284bb00a043ebbd5d3893c24a4613bb2539
ANA JULLYA MIRANDA DOS SANTOS <anajullyamirandadossantos@gmail.com>
2025-11-24 13:39:26 -0300

    Add files via upload

---
+++ TRABALHOCALCULO/TRABALHOCALCULO/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace TRABALHOCALCULO
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtLadoA;
+        private System.Windows.Forms.TextBox txtLadoB;
+        private System.Windows.Forms.TextBox txtLadoC;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLadoA = new System.Windows.Forms.TextBox();
+            this.txtLadoB = new System.Windows.Forms.TextBox();
+            this.txtLadoC = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLadoA
+            // 
+            this.txtLadoA.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtLadoA.Location = new System.Drawing.Point(141, 35);
+            this.txtLadoA.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtLadoA.Name = "txtLadoA";
+            this.txtLadoA.Size = new System.Drawing.Size(326, 23);
+            this.txtLadoA.TabIndex = 0;
+            // 
+            // txtLadoB
+            // 
+            this.txtLadoB.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtLadoB.Location = new System.Drawing.Point(140, 81);
+            this.txtLadoB.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtLadoB.Name = "txtLadoB";
+            this.txtLadoB.Size = new System.Drawing.Size(326, 23);
+            this.txtLadoB.TabIndex = 1;
+            // 
+            // txtLadoC
+            // 
+            this.txtLadoC.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtLadoC.Location = new System.Drawing.Point(141, 130);
+            this.txtLadoC.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtLadoC.Name = "txtLadoC";
+            this.txtLadoC.Size = new System.Drawing.Size(326, 23);
+            this.txtLadoC.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCalcular.ForeColor = System.Drawing.Color.White;
+            this.btnCalcular.Location = new System.Drawing.Point(140, 173);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(327, 40);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblResultado.Location = new System.Drawing.Point(35, 236);
+            this.lblResultado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(443, 29);
+            this.lblResultado.TabIndex = 4;
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTipo.ForeColor = System.Drawing.Color.DarkBlue;
+            this.lblTipo.Location = new System.Drawing.Point(35, 265);
+            this.lblTipo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(443, 29);
+            this.lblTipo.TabIndex = 5;
+            // 
+            // lblArea
+            // 
+            this.lblArea.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblArea.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblArea.Location = new System.Drawing.Point(35, 294);
+            this.lblArea.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(443, 29);
+            this.lblArea.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(35, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 23);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Lado A:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(35, 84);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 23);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Lado B:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(35, 130);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 23);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Lado C:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(506, 359);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtLadoC);
+            this.Controls.Add(this.txtLadoB);
+            this.Controls.Add(this.txtLadoA);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de Triângulos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
+
+        #endregion
---
+++ TRABALHOCALCULO/TRABALHOCALCULO/Form1.cs
@@ -0,0 +1,95 @@
+﻿namespace TRABALHOCALCULO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Lê os valores dos lados
+                double ladoA = double.Parse(txtLadoA.Text);
+                double ladoB = double.Parse(txtLadoB.Text);
+                double ladoC = double.Parse(txtLadoC.Text);
+
+                // Verifica se forma um triângulo válido
+                if (!EhTrianguloValido(ladoA, ladoB, ladoC))
+                {
+                    lblResultado.Text = "❌ Estes lados NÃO formam um triângulo!";
+                    lblResultado.ForeColor = System.Drawing.Color.Red;
+                    lblTipo.Text = "";
+                    lblArea.Text = "";
+                    return;
+                }
+
+                lblResultado.Text = "✓ É um triângulo válido!";
+                lblResultado.ForeColor = System.Drawing.Color.DarkGreen;
+
+                // Determina o tipo do triângulo
+                string tipo = DeterminarTipo(ladoA, ladoB, ladoC);
+                lblTipo.Text = "Tipo: " + tipo;
+
+                // Calcula a área usando Fórmula de Heron
+                double area = CalcularArea(ladoA, ladoB, ladoC);
+                lblArea.Text = string.Format("Área: {0:F2} unidades²", area);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, digite valores numéricos válidos!",
+                    "Erro de Entrada", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message,
+                    "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private bool EhTrianguloValido(double a, double b, double c)
+        {
+            // Verifica se os lados são positivos
+            if (a <= 0 || b <= 0 || c <= 0)
+                return false;
+
+            // Verifica a desigualdade triangular
+            return (a + b > c) && (a + c > b) && (b + c > a);
+        }
+
+        private string DeterminarTipo(double a, double b, double c)
+        {
+            if (a == b && b == c)
+            {
+                return "🔺 Equilátero (todos os lados iguais)";
+            }
+            else if (a == b || b == c || a == c)
+            {
+                return "🔺 Isósceles (dois lados iguais)";
+            }
+            else
+            {
+                return "🔺 Escaleno (todos os lados diferentes)";
+            }
+        }
+
+        private double CalcularArea(double a, double b, double c)
+        {
+            // Fórmula de Heron
+            double s = (a + b + c) / 2; // semi-perímetro
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            return area;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
